d0c1cc73dbd9afa8e364f60cfbba79b461825721
Oscar Lin <106703781+Unbrokensmile@users.noreply.github.com>
2022-06-12 20:04:26 +0000

    add day2 folder

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-print ("Welcome to the tip calculator ")
-money = input("What was the total bill? $$$")
-money = float(money)
-tip_p = input("What precentage of the tip whold you like to give? *just the number")
-tip_p = int(tip_p)
-tip_p2 = tip_p / 100 * money
-money2 = money + tip_p2
-split1 = input("How many people split the bill?")
-split1 = int(split1)
-money2 = round(money2 / split1, 2)
-print(f"each of you spend{money2} each")
+import day2.split_bill
